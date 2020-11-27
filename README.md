e7ea9bb5e126613ad652678ccdb7e3ef4f8a8053
mati793 <75143808+mati793@users.noreply.github.com>
2020-11-27 21:19:31 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,76 @@
+![image description](https://cdn.discordapp.com/attachments/724777338474201099/770879411846250546/0360.png)
+
+# **All For One Bot**
+
+All For One Bot is a free and open source solution to your server managing, built with quality in mind. Our goal is to cover as many functionalities and niches as possible, while still maintaining quality. This project is still under developement. If You run into any issues please feel free to let me know im happy to help you out.
+
+Join our discord server ➡️ [All For One SL](https://discord.gg/UstfHPG)
+
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://github.com/BlazeIsClone/A41SLBOT/commits/master)
+[![GitHub contributors](https://img.shields.io/github/contributors/Naereen/StrapDown.js.svg)](https://github.com/BlazeIsClone/A41SLBOT/graphs/contributors/)
+[![Discord](https://img.shields.io/discord/591914197219016707.svg?label=&logo=discord&logoColor=ffffff&color=7389D8&labelColor=6A7EC2)](https://discord.com/invite/UstfHPG)
+[![Generic badge](https://img.shields.io/badge/Instructions-wiki-<COLOR>.svg)](https://github.com/BlazeIsClone/A41SLBOT/wiki/)
+[![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)](https://github.com/BlazeIsClone/A41SLBOT/blob/master/LICENSE.txt)
+
+<hr>
+
+## 📥 Installation
+
+For self hosting clone this repository
+
+```bash
+git clone https://github.com/BlazeIsClone/A41SLBOT.git
+```
+
+For the code to work you need to setup the environment variables reffer to the wiki to setup these.
+[✔️ wiki](https://github.com/BlazeIsClone/A41SLBOT/wiki)
+
+## 🚧 Requesting Features [![Generic badge](https://img.shields.io/badge/Request-Feature-<COLOR>.svg)](https://github.com/BlazeIsClone/A41SLBOT/issues/new?assignees=&labels=&template=feature_request.md&title=)
+
+To request features open a new issue in this repository.
+
+**Is your feature request related to a problem? Please describe.**
+
+A clear and concise description of what the problem is. Ex. I'm always frustrated when [...]
+
+**Describe the solution you'd like**
+
+A clear and concise description of what you want to happen.
+
+**Describe alternatives you've considered**
+
+A clear and concise description of any alternative solutions or features you've considered.
+
+**Additional context**
+
+Add any other context or screenshots about the feature request.
+
+# 👏 Contributor Covenant Code of Conduct
+
+All For One Bot is still on alpha stage contribution would be appreciated ❤️.
+
+## 🔒 Our Responsibilities
+
+Project maintainers are responsible for clarifying the standards of acceptable
+behavior and are expected to take appropriate and fair corrective action in
+response to any instances of unacceptable behavior.
+
+Project maintainers have the right and responsibility to remove, edit, or
+reject comments, commits, code, wiki edits, issues, and other contributions
+that are not aligned to this Code of Conduct, or to ban temporarily or
+permanently any contributor for other behaviors that they deem inappropriate,
+threatening, offensive, or harmful.
+
+## ✉️ Attribution
+
+This Code of Conduct is adapted from the [Contributor Covenant][homepage], version 1.4,
+available at https://www.contributor-covenant.org/version/1/4/code-of-conduct.html
+
+[homepage]: https://www.contributor-covenant.org
+
+For answers to common questions about this code of conduct, see
+https://www.contributor-covenant.org/faq
+
+## 📜 License [![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)](https://github.com/BlazeIsClone/A41SLBOT/blob/master/LICENSE.txt)
+
+[📄 GNU General Public License v3.0](https://github.com/BlazeIsClone/A41SLBOT/blob/master/LICENSE.txt)
---
+++ index.js
@@ -0,0 +1,167 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { Collection } = require("discord.js");
+const events = (require("events").EventEmitter.defaultMaxListeners = 35);
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const { PREFIX } = require("./config.json");
+const config = require("./src/database/roles-reaction.json");
+const nsfwConfig = require("./config.json");
+const fs = require("fs");
+const moment = require("moment");
+moment.locale("fr");
+require("dotenv").config();
+const TOKEN = process.env.DISCORD_TOKEN;
+
+const image = require("./src/commands/misc/image");
+const eval = require("./src/commands/dev/eval");
+const ping = require("./src/commands/ping");
+const queue = require("./src/commands/music/play.js");
+const help = require("./src/commands/help");
+const serverInfo = require("./src/commands/serverInfo");
+const rules = require("./src/commands/rules");
+const leaveVoice = require("./src/commands/leave");
+const joinVoice = require("./src/commands/join");
+const welcome = require("./src/listeners/welcome");
+const goodbye = require("./src/listeners/goodbye");
+const statusPresence = require("./src/listeners/statusPresence");
+const memberCount = require("./src/listeners/member-count");
+const reactionRoles = require("./src/commands/admin/reactionRoles");
+const musicCommands = require("./src/commands/musicCommands");
+const memes = require("./src/commands/memes/memes");
+const announcementsWebhook = require("./src/webhooks/Announcements");
+const bulkDelete = require("./src/commands/admin/bulkDelete");
+const nasa = require("./src/commands/misc/nasa");
+
+image(client);
+eval(client);
+ping(client);
+help(client);
+serverInfo(client);
+rules(client);
+leaveVoice(client);
+joinVoice(client);
+welcome(client);
+goodbye(client);
+statusPresence(client);
+memberCount(client);
+reactionRoles(client);
+musicCommands(client);
+memes(client);
+announcementsWebhook(client);
+bulkDelete(client);
+nasa(client);
+
+client.commands = new Collection();
+client.prefix = PREFIX;
+client.queue = new Map();
+const cooldowns = new Collection();
+const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+const commandFiles = readdirSync(
+    join(__dirname, "./src/commands/music/")
+).filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+    const command = require(join(
+        __dirname,
+        "./src/commands/music/",
+        `${file}`
+    ));
+    client.commands.set(command.name, command);
+}
+
+client.on("message", async (message) => {
+    if (message.author.bot) return;
+    if (!message.guild) return;
+
+    const prefixRegex = new RegExp(
+        `^(<@!?${client.user.id}>|${escapeRegex(PREFIX)})\\s*`
+    );
+    if (!prefixRegex.test(message.content)) return;
+
+    const [, matchedPrefix] = message.content.match(prefixRegex);
+
+    const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command =
+        client.commands.get(commandName) ||
+        client.commands.find(
+            (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+        );
+
+    if (!command) return;
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 1) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime =
+            timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(
+                `please wait ${timeLeft.toFixed(
+                    1
+                )} more second(s) before reusing the \`${
+                    command.name
+                }\` command.`
+            );
+        }
+    }
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+    try {
+        if (typeof command.execute === "function") {
+            command.execute(message, args);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+client.config = nsfwConfig;
+client.on("message", function (message) {
+    if (message.author.bot) return;
+    if (message.content.indexOf(nsfwConfig.nsfwPrefix) !== 0) return;
+    const args = message.content
+        .slice(nsfwConfig.nsfwPrefix.length)
+        .trim()
+        .split(/ +/g);
+    const command = args.shift().toLowerCase();
+    const cmd = client.commands.get(command);
+    if (!cmd) return;
+    cmd.run(client, message, args);
+});
+
+fs.readdir("./src/commands/nsfw/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./src/commands/nsfw/${file}`);
+        let commandName = file.split(".")[0];
+        // console.log(`Load command ${commandName}`);
+        client.commands.set(commandName, props);
+    });
+});
+fs.readdir(`./src/listeners/`, (err, files) => {
+    if (err) return console.error(err);
+    files.forEach((file) => {
+        let eventFunction = require(`./src/listeners/${file}`);
+        let eventName = file.split(".")[0];
+        client.on(eventName, (...args) => eventFunction.run(client, ...args));
+    });
+});
+
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e));
+
+client.login(TOKEN);
